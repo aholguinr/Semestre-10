0f3c3dfbc57138b47c86d9438b0d4e0f2a64f0af
aholguinr <aholguinr@unal.edu.co>
2023-04-19 19:00:42 -0500

    SCADA PAI

---
+++ PAI/Control/MPLABX/PAI.X/build/default/production/PAI.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/PAI.d  \
+ build/default/production/PAI.p1:  \
+ PAI.c  \
+LibLCDXC8.h 
--- PAI/Proyectos.X/Lab4.X/build/default/production/Lab4.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/Lab4.d  \
- build/default/production/Lab4.p1:  \
- Lab4.c  \
-LibLCDXC8.h 
