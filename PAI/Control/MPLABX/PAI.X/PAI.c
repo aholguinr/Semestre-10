04bf688c20010f33ece7e48d69af6e5dfdd70f94
aholguinr <aholguinr@unal.edu.co>
2023-05-09 22:22:52 -0500

    mucho todo

--- PAI/Control/MPLABX/PAI.X/PAI.c
+++ PAI/Control/MPLABX/PAI.X/PAI.c
@@ -7,11 +7,12 @@
 
 
 char Estados=0;
+char EstadosPrev=0;
 char a=0;
 char b=0;
 char c=0;
 char d=0; //Motor apagado
-
+double DutyI, DutyF,Duty, DeltaDuty,DutyF2;
 void main(void){
     
     //Configuraciones de pines
@@ -24,15 +25,28 @@ void main(void){
 
     CCP1CON=0b00001100; //Implementación PWM
     CCP2CON=0b00001100; //Implementación PWM 
-    PR2=124;            //PWM de 1kHz
+    //PR2=124;            //PWM de 1kHz
+    PR2=249;
     CCPR1L=0;           
     CCPR2L=0;           //Duty inicial de 0
+    T2CON=0b00000000;   //Postscaler 1:1, Off, Prescaler 1;
+    TMR2=0;             //Prescaler TMR2
+    TMR2ON=1;           //Prender TMR2 
     
     TRISC1=0;           //Salida PWM2
     TRISC2=0;           //Salida PWM1 enable LM293D
     
+    
+    
     //Interrupciones
+    
+    
     /*
+    T1CON=0b10100001;   //TMR1 config
+    TMR1=3036;          //TMR1 precarga
+    TMR1IF=0;           //TMR1 flag  
+    TMR1IE=1;           //TMR1 enable 
+    */
     INT0IF=0;
     INT0IE=1;
     
@@ -40,16 +54,15 @@ void main(void){
     INT1IE=1;
     INT2IF=0;
     INT2IE=1;
-    RBPU=1;*/
+    RBPU=1;
     GIE=1;              //Activar interrupciones globales
     PEIE=1;             //Activar interrupciones de perifericos        
             
  
     
-    a=!RB0;
-    b=!RB1;
-    c=!RB2;
-    d=!RB3;
+    a=!RB0; //Energia motor
+    
+    d=!RB3; //OP
     
     ConfiguraLCD(4);
     InicializaLCD();
@@ -59,52 +72,77 @@ void main(void){
     while(1){
         
         a=!RB0;
-        b=!RB1;
-        c=RB2;
+        
         d=!RB3;
         
  
         switch(Estados){
             
             case 0:
-                Mensaje_LCD_Var_Mensaje20x4("Cortadora UN","Espere un momento","UNAL","2023-1");
                 LATD=0;
-                __delay_ms(2000);
+                Mensaje_LCD_Var_Mensaje20x4("Cortadora UN","Espere un momento","UNAL","2023-1");
                 Estados=1;
+                __delay_ms(2000);
                 break;
                 
             case 1:
-                Mensaje_LCD_Var_Mensaje20x4(" ","Revise la conexion a ","los motores","  ");
                 
                 RD0=0;
+                RD1=0;
+                CCPR1L=0;
+                c=0;
+                if(Estados!=EstadosPrev){    
+                Mensaje_LCD_Var_Mensaje20x4(" ","Revise la conexion a ","los motores","  ");
+                }
+                EstadosPrev=1;
                 if(a==0){Estados=1;break;}//Motor desconectado
                 else{Estados=2;break;} //Pasar a Motor OFF
                 
                 break;
             case 2:
-                Mensaje_LCD_Var_Mensaje20x4("Motor OFF","  ","Active el pulsador","para ajustarlo");
  
                 RD0=1;
+                RD1=0;
+                b=1;
+                c=0;
+                CCPR1L=0;
+                if(Estados!=EstadosPrev){    
+                Mensaje_LCD_Var_Mensaje20x4("Motor OFF","  ","Active el pulsador","para ajustarlo");
+                }
+                
+                EstadosPrev=2;
                 if(a==0){Estados=1;break;}//Motor desconectado
-                if(d==1){Estados=3;break;}//Pasar a OP
+                if(d==1){Estados=3;b=0;break;}//Pasar a OP
                 else{Estados=2;break;}
                 break;
                 
             case 3:
-                Mensaje_LCD_Var_Mensaje20x4("Seleccione un %","de operacion:"," ","0%");
+                RD1=0;
                 
-                
+                Duty=1;
+                CCPR1L=0;
                 if(a==0){Estados=1;break;}//Motor desconectado
                 //if(a==0){Estados=2;break;}
-                if(b==1){Estados=2;break;}//Apagar motor
-                if(c==1){Estados=4;break;}//Prender motor
+                if(Estados!=EstadosPrev){    
+                Mensaje_LCD_Var_Mensaje20x4("Seleccione un %","de operacion:"," ","0%");
+                }
+                
+                EstadosPrev=3;
+                if(b==1){Estados=2;b=0;break;}//Apagar motor
+                if(c==1){Estados=4;c=0;break;}//Prender motor
                 break;
                 
             case 4:
+                RD1=1;
+                CCPR1L=Duty*PR2;
+                if(Estados!=EstadosPrev){    
                 Mensaje_LCD_Var_Mensaje20x4("Motor prendido","Operacion:"," ","0%");
+                }
                 
+                EstadosPrev=4;
+                //CCPR2L=Duty*PR2;
                 if(a==0){Estados=1;break;}//Motor desconectado
-                if(b==1){Estados=2;break;}//Apagar motor
+                if(b==1){Estados=2;b=0;break;}//Apagar motor
                 //if(b==0){Estados=2;break;}
                 break;
                 
@@ -121,18 +159,25 @@ void main(void){
 
 void interrupt ISR(void){
     
-    /*
+    
+    if(INT0IF==1){ //Apagar motor
+
+        a=0;
+        INT0IF=0;
+    }
+    
     if(INT1IF==1){ //Apagar motor
 
-        b=!RB1;
+        b=1;
         INT1IF=0;
     }
     
+    
     if(INT2IF==1){ //Prender motor
 
-        c=RB2;
+        c=1;
         INT2IF=0;
-    }*/
+    }
     
 
     
